0ee6917c9b78e72c41c83597f1926eecc27c8f75
ReitoSerizawa <s.reitiger@gmail.com>
2022-05-18 22:39:50 -0400

    commit tree d72256078dda3193bf16a0edcad181c3a3c6074f

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,64 @@
 # your code goes here
+
+# Person class will have the ability to:
+
+# Get paid/receive payments
+# Take a bath
+# Call a friend
+# Start a conversation
+# State if they are happy and/or clean
+
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(happiness)
+        if happiness > 10
+            @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        else @happiness = happiness
+        end
+    end
+
+    def hygiene=(hygiene)
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        else @hygiene = hygiene
+        end
+    end
+
+    def clean?
+        @hygiene > 7 ? true : false
+    end
+
+    def happy?
+        @happiness > 7 ? true : false
+    end
+
+    def get_paid (amount)
+        @bank_account = bank_account + amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene = hygiene + 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness = happiness + 2
+        self.hygiene = hygiene - 3
+        "♪ another one bites the dust ♫"
+    end
+
+end
